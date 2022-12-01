5e2ca9f3e9d0e0017a07680a2c338604d9f8fb60
Trọng Nhân Nguyễn Lê <trongnhanofficial@gmail.com>
2022-12-02 01:42:28 +0700

    Load map - NLTN

--- app/src/main/java/com/example/iotremote/MainActivity.java
+++ app/src/main/java/com/example/iotremote/MainActivity.java
@@ -1,26 +1,73 @@
 package com.example.iotremote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
 
+//import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private MapView map;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(
+                getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+        );
+        setContentView(R.layout.load_map);
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        GeoPoint startPoint = new GeoPoint(10.86815, 106.79931);
+        IMapController mapController = map.getController();
+        mapController.setZoom(14.0);
+        mapController.setCenter(startPoint);
 
-        setContentView(R.layout.quen_mat_khau);
-        //mmm
-        //nnnn
-        //ngoc 12341421
-        //333333
-        //Pham Van Thang Nhu trai
-        //444444
+        ArrayList<OverlayItem> items = new ArrayList<>();
+        OverlayItem home = new OverlayItem("Rallo's office", "my office", new GeoPoint(10.87304, 106.80524));
+        Drawable m = home.getMarker(0);
+        items.add(home);
+        items.add(new OverlayItem("Resto", "chez babar", new GeoPoint(10.87304, 106.80524)));
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(), items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            @Override
+            public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                return true;
+            }
 
-        setContentView(R.layout.map);
+            @Override
+            public boolean onItemLongPress(int index, OverlayItem item) {
+                return false;
+            }
+        });
+        mOverlay.setFocusItemsOnTap(true);
+        map.getOverlays().add(mOverlay);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        map.onPause();
+    }
 
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        map.onResume();
     }
 
 }
