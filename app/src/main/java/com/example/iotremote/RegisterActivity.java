5775a5d31a5a222a1a9f0c60f41964753e09a2f6
EnujV <20522171@gm.uit.edu.vn>
2022-12-31 14:46:36 +0700

    fb login + gg login

--- app/src/main/java/com/example/iotremote/RegisterActivity.java
+++ app/src/main/java/com/example/iotremote/RegisterActivity.java
@@ -1,18 +1,34 @@
 package com.example.iotremote;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import android.widget.ImageButton;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.iotremote.assetdetail_database.DatabaseHandler;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import com.example.iotremote.MainActivity;
+import com.example.iotremote.R;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -23,18 +39,46 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Arrays;
+
 public class RegisterActivity extends AppCompatActivity {
     DatabaseReference dtbasereference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://iotz-ce511-default-rtdb.firebaseio.com/");
+    CallbackManager callbackManager;
+    GoogleSignInClient gsc;
     @Override
     protected  void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();final EditText email = findViewById(R.id.et_email);
-       final EditText confirmpassword = findViewById(R.id.et_password_2);
-       final EditText username = findViewById(R.id.et_4);
-       final EditText password = findViewById(R.id.tv_5);
-       final Button btn_sign_up = findViewById(R.id.btn_sign_up);
-       final Button btn_sign_in_now = findViewById(R.id.btn_sign_in_now);
+        callbackManager = CallbackManager.Factory.create();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        final EditText email = findViewById(R.id.et_email);
+        final EditText confirmpassword = findViewById(R.id.et_password_2);
+        final EditText username = findViewById(R.id.et_4);
+        final EditText password = findViewById(R.id.tv_5);
+        final Button btn_sign_up = findViewById(R.id.btn_sign_up);
+        final Button btn_sign_in_now = findViewById(R.id.btn_sign_in_now);
+        final ImageButton btn_fb = findViewById(R.id.btn_facebook);
+        final ImageButton btn_google = findViewById(R.id.btn_google_sign_in);
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                        finish();
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
         btn_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -83,7 +127,56 @@ public class RegisterActivity extends AppCompatActivity {
             }
 
         });
+        btn_fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logInWithReadPermissions(RegisterActivity.this, Arrays.asList("public_profile"));
+            }
+        });
+        btn_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
 
+
+
+    }
+    void signIn(){
+        Intent signinintent = gsc.getSignInIntent();
+        startActivityForResult(signinintent,100);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==100){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this );
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+                Toast.makeText(this, "User email :"+personEmail, Toast.LENGTH_SHORT).show();
+            }
+            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+
+            Log.d("Message", e.toString());
+
+        }
     }
 
 
